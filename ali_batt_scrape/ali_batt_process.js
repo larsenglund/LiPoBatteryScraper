af5947326d08d29d128bb438d2ed7b5c70297117
Lars Englund <lars.englund@gmail.com>
2016-02-23 10:02:42 +0100

    Update ali_batt_process.js

--- ali_batt_scrape/ali_batt_process.js
+++ ali_batt_scrape/ali_batt_process.js
@@ -1,8 +1,7 @@
 var request = require('request');
-//var quiche = require('quiche');
-// https://github.com/mbostock/d3/wiki/Gallery
 var fs = require('fs');
-var plotly = require('plotly')("englund", "ocwj99o160");
+// https://github.com/mbostock/d3/wiki/Gallery
+var plotly = require('plotly')("englund", "use your own please :)");
 
 var obj = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
 console.log(obj.length);
@@ -49,31 +48,3 @@ var graphOptions = {layout: layout, filename: "basic-line", fileopt: "overwrite"
 plotly.plot(data, graphOptions, function (err, msg) {
     console.log(msg);
 });
-
-/*var chart = quiche('line');
-//chart.setWidth(10000);
-//chart.setHeight(1000);
-chart.setTitle('Aliexpress Batteries');
-chart.addData([3000, 2900, 1500], 'Blah', '008000');
-chart.addData([1000, 1500, 2000], 'Asdf', '0000FF');
-//chart.addAxisLabels('x', ['1800', '1900', '2000']);
-chart.setAutoScaling();
-//chart.setTransparentBackground();
-chart.setLegendBottom(); // Put legend at bottom
-
-var imageUrl = chart.getUrl(true); // First param controls http vs. https
-console.log(imageUrl);
-
-var download = function(uri, filename, callback){
-  request.head(uri, function(err, res, body){
-    console.log('content-type:', res.headers['content-type']);
-    console.log('content-length:', res.headers['content-length']);
-
-    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-  });
-};
-
-download(imageUrl, 'google.png', function(){
-  console.log('done');
-});
-*/
